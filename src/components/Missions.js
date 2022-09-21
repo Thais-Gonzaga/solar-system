c3306f16c50b6ee9978b490f7b5cda89c1506530
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-09-21 18:58:00 -0300

    feat: Renderizando o componente Title dentro do componente Missions.
     #ref: requisito 8

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions" />
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
 
     );
   }
