83378b307cc37f9e49b03d31a91d5e8c834dfdbd
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-09-21 18:50:47 -0300

    feat: Renderizando lista com os planetas no SolarSystem
     #ref:requisito 6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
+// import Title from './components/Title';
 
 class App extends React.Component {
   render() {
@@ -9,7 +9,6 @@ class App extends React.Component {
       <>
         <Header />
         <SolarSystem />
-        <Title headline="headlineText" />
       </>
     );
   }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
+    console.log(planets);
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map(({ name, image }) => (
+          <li key={ image }>
+            <PlanetCard planetName={ name } planetImage={ image } />
+          </li>
+        ))}
       </div>
 
     );
