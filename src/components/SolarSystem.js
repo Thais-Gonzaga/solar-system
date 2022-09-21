056a2fb0930b5585ce3464a1412ac900dd32aec8
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-09-21 16:13:06 -0300

    feat: Criando um componente chamado SolarSystem
     #ref: requisito 2

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system" />
+
+    );
+  }
+}
+
+export default SolarSystem;
